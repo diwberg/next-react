00e8bec4c4681f1d472cdc30995033ffcdb8dd62
Diwberg <diwberg@hotmail.com>
2023-06-05 22:13:17 +0000

    exercicios api

--- exercicios/pages/apis.jsx
+++ exercicios/pages/apis.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
-export default function rotas(){
+
+export default function apis(){
     return (
         <div style={{
             display: "flex",
@@ -7,8 +8,9 @@ export default function rotas(){
             alignItems: "center",
             gap: "10px"
         }}>
-            <h1>Rotas</h1>
-            <Link href="/rotas/products"><button>Produtos</button></Link>
+            <h1>APIs</h1>
+            <Link href="/api/hello"><button>API do Next</button></Link>
+            <Link href="/api/types"><button>Tipo de Requisição</button></Link>
             <Link href="/"><button>Home</button></Link>
         </div>
     )
--- exercicios/pages/rotas/products.jsx
+++ exercicios/pages/rotas/products.jsx
@@ -1,9 +1,25 @@
 import Link from 'next/link'
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 export default function products(){
     const [idProduto, setIdProduto] = useState(0)
-    const [param, setParam] = useState(0)
+
+    const router = useRouter()
+
+    function handlePushToRotas(){
+        router.push("/rotas")
+    }
+
+    function handlePushWithParams(){
+        router.push({
+            pathname: "/rotas/id/produto",
+            query: {
+                param1: "param1",
+                param2: "param2"
+            }
+        })
+    }
 
     return (
         <div style={{
@@ -16,8 +32,9 @@ export default function products(){
                 height: "60vh",
                 background: "grey",
                 padding: "15px",
-
-                
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px"
             }}>
                 <div>
                 <input type="text" placeholder='Coloque um id' onChange={e => setIdProduto(e.target.value)}  />
@@ -29,7 +46,34 @@ export default function products(){
                     <input type="text" name='param2' placeholder='param2' />
                     <button type="submit">Enviar</button>
                 </form>
-                </div>                
+                </div>
+
+                <div style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <h4 style={{
+                    color: "white",
+                    padding: "10px"
+                }}>Navegando entre páginas</h4>
+
+                <ul style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "15px"
+                }}>
+                    <li>A navegação por meio do <code>push</code> simples</li>
+                    <button onClick={handlePushToRotas}>Push rotas</button>
+
+                    <li>A navegação por meio do <code>push</code> com Params</li>
+                    <button onClick={handlePushWithParams}>Push Params</button>
+
+                </ul>
+
+                </div>
+
             </div>
 
             <Link href="/rotas"><button>Voltar</button></Link>
